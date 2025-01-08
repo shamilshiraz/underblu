55963863efde879c2b265d659c3f7a3f8e6894aa
shamilshiraz <shamilshiraz0@gmail.com>
2025-01-09 00:36:54 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-      <div className='bg-[url(../blu.png)] h-[100vh] bg-cover  w-full grid place-content-center flex-end flex '>
+      <div className='bg-[url(../blu.webp)] h-[100vh] bg-cover  w-full grid place-content-center flex-end flex '>
       <svg data-aos="fade-up"  id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 394.44 138.94" className='sm:h-[50vh] h-[10vh] sm:mt-[300px] '>
 
   <g id="Layer_1-2" data-name="Layer_1">
